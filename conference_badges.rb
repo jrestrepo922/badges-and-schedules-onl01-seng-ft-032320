e50140bc7ba8b635d312ce0b905eb85a67c9727a
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-25 15:59:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}." 
+end 
+
+
+
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push(badge_maker(name))
+  end 
+  new_array
+end 
+
+
+
+def assign_rooms(speakers)
+  room_speakers = []
+  speakers.each_with_index do |speaker, index|
+    room_speakers.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+  end
+  room_speakers
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attende|
+    puts attende
+  end 
+  assign_rooms(attendees).each do |attende|
+    puts attende
+  end 
+end 
+
